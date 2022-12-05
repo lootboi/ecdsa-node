c4ab7c2d6c8c9e4db340ca9aecbf560438fd0d45
daviduke <katora@dons.usfca.edu>
2022-12-05 01:31:03 -0700

    integrated ethereum addresses

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -2,17 +2,23 @@ import server from "./server";
 
 import * as secp from "ethereum-cryptography/secp256k1";
 import { toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
+
+function getAddress(publicKey) {
+    let hash = keccak256(publicKey.slice(1));
+    return toHex(hash.slice(-20));
+};
 
 function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
     const privateKey = evt.target.value;
     setPrivateKey(privateKey);
-    const address = toHex(secp.getPublicKey(privateKey));
-    setAddress(address);
+    const address = getAddress(secp.getPublicKey(privateKey));
+    setAddress('0x' + address);
     if (address) {
       const {
         data: { balance },
-      } = await server.get(`balance/${address}`);
+      } = await server.get(`balance/${'0x'+address}`);
       setBalance(balance);
     } else {
       setBalance(0);
@@ -28,7 +34,7 @@ function Wallet({ address, setAddress, balance, setBalance, privateKey, setPriva
         <input placeholder="Type a Private Key..." value={privateKey} onChange={onChange}></input>
       </label>
       <div>
-        Address: {address.slice(0, 5)}...{address.slice(-5)}
+        Address: {address.slice(0,5)}...{address.slice(-5)}
       </div>
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,9 +1,21 @@
 const secp = require('ethereum-cryptography/secp256k1');
 const { toHex } = require('ethereum-cryptography/utils');
+const { keccak256 } = require("ethereum-cryptography/keccak");
 
+// Function to hash the public key generated into an ethereum format address
+function getAddress(publicKey) {
+    let hash = keccak256(publicKey.slice(1));
+    return hash.slice(-20);
+};
+
+// Constants defining the private key, public key, and ethAddress generated from both
 const privateKey = secp.utils.randomPrivateKey();
 const publicKey = secp.getPublicKey(privateKey);
+const ethAddress = '0x' + toHex(getAddress(publicKey));
 
+// Print all of the keys and address generated
 console.log('Private key:', toHex(privateKey));
 console.log('Public key:', toHex(publicKey));
+console.log('Ethereum address:', ethAddress);
 
+// 
